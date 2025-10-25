2f4df8d9e6459352c58a45515a1583d98f6d7381
Medoss22 <medm82050@gmail.com>
2025-10-25 15:14:52 +0100

    fixed

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 checkGuess()
 {
-	if ["$1" -lt "$filesNumber"]; then
+	if [ "$1" -lt "$filesNumber" ]; then
 		echo "too low, try again"
 	elif [ "$1" -gt "$filesNumber" ]; then
 		echo "too high, try again"
@@ -11,10 +11,14 @@ checkGuess()
 	fi
 }
 
-filesNumber = $(ls -1 | wc -1)
+filesNumber=$(ls -1 | wc -l)
 echo "guess the number of files in this repo"
 
 while true; do
 	read -p "enter you number: " guess
-	checkGuess $guess
+	if ! [[ "$guess" =~ ^[0-9]+$ ]]; then
+		echo "thas not a number"
+		continue
+	fi
+	checkGuess "$guess"
 done
